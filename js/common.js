6c09780f648198b56a30c3c52bc34fe904b2b4d2
shubhoahmed <shubhoahmed03@gmail.com>
2022-08-22 10:04:57 +0600

    budget part added

--- js/common.js
+++ js/common.js
@@ -1,1 +1,19 @@
-console.log('commmon js working')
+function getInputFieldValueByID(inputId) {
+    const inputField = document.getElementById(inputId);
+    const perPlayerBudgetString = inputField.value;
+    const perPlayerBudget = parseInt(perPlayerBudgetString);
+    return perPlayerBudget;
+}
+
+function getTextValueByID(elementId) {
+    const textElementValue = document.getElementById(elementId);
+    const totalPlayerExpenseString = textElementValue.innerText;
+    const totalPlayerExpense = parseInt(totalPlayerExpenseString);
+    return totalPlayerExpense;
+}
+
+function setvalueById(elementId, newValue) {
+    const totalCost = document.getElementById(elementId);
+    totalCost.innerText = newValue;
+    return totalCost;
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,67 @@
+const playersArray = [];
+
+function displayPlayersName(selectedName) {
+    const tableBody = document.getElementById('player-name');
+
+    if (selectedName.length > 5) {
+        alert('You cannot select more than 5 player. Please reload and select again less then 5 or equal five player for get total player expense calculation');
+        return;
+    }
+
+    tableBody.innerHTML = '';
+
+    for (let i = 0; i <= 5; i++) {
+
+        const name = playersArray[i].Name;
+
+        const tr = document.createElement('tr');
+
+        tr.innerHTML = `
+        <th>${i + 1}</th>
+        <td>${name}</td>
+        `
+        tableBody.appendChild(tr);
+
+    }
+}
+
 function addFavouritePlayer(element) {
-    console.log(element.parentNode);
+    // console.log(element.parentNode.parentNode.children[0].innerText);
+    const playerName = element.parentNode.parentNode.children[0].innerText;
+
+    const playerObject = {
+        Name: playerName
+    }
+
+    playersArray.push(playerObject);
+
+    displayPlayersName(playersArray);
 };
+
+document.getElementById('btn-calculate').addEventListener('click', function () {
+    const perPlayerValue = getInputFieldValueByID('input-field');
+
+    const totalExpense = perPlayerValue * playersArray.length;
+
+    const totalPlayerExpense = getTextValueByID('player-expense');
+
+    const expenseTotal = setvalueById('player-expense', totalExpense);
+    expenseTotal.innerText = totalExpense;
+
+
+})
+
+document.getElementById('btn-total-calculate').addEventListener('click', function () {
+
+    const managerInputField = getInputFieldValueByID('manager-field');
+    const coachInputField = getInputFieldValueByID('coach-field');
+    const totalCost = getTextValueByID('total-cost');
+
+    const totalPlayerExpense = getTextValueByID('player-expense');
+
+    const total = managerInputField + coachInputField + totalPlayerExpense;
+
+    const finalTotal = setvalueById('total-cost', total);
+    finalTotal.innerText = total;
+
+})
